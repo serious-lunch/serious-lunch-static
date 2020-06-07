3d0b00481bd9f27e9d5aefd412edba4968f1fc84
dyoshimitsu <d.yoshimitsu@gmail.com>
2020-06-07 19:42:49 +0900

    add Bulma sample

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,18 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (Html, div, h1, p, section, strong, text)
+import Html.Attributes exposing (class)
 
 
+main : Html msg
 main =
-    text "Hello World!"
+    section [ class "section" ]
+        [ div [ class "container" ]
+            [ h1 [ class "title" ] [ text "Hello World!" ]
+            , p [ class "subtitle" ]
+                [ text "My first website with "
+                , strong [] [ text "Bulma" ]
+                , text "!"
+                ]
+            ]
+        ]
