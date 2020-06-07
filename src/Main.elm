4db4db0eaa6c91edd102ebf0c041a72b3490f4e6
dyoshimitsu <d.yoshimitsu@gmail.com>
2020-06-07 21:18:52 +0900

    add nav and footer

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,19 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, h1, p, section, strong, text)
+import Html exposing (Html, div, footer, h1, nav, p, section, strong, text)
 import Html.Attributes exposing (class)
 
 
 main : Html msg
 main =
-    section [ class "section" ]
-        [ div [ class "container" ]
-            [ h1 [ class "title" ] [ text "Welcome to the Serious Lunch" ]
-            , p [ class "subtitle" ]
-                [ text "This is the home page for the lunch information sharing service." ]
+    div []
+        [ nav [] []
+        , section [ class "section" ]
+            [ div [ class "container" ]
+                [ h1 [ class "title" ] [ text "Welcome to the Serious Lunch" ]
+                , p [ class "subtitle" ]
+                    [ text "This is the home page for the lunch information sharing service." ]
+                ]
             ]
+        , footer [ class "footer" ] []
         ]
